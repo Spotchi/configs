203c888bd2dd9e9eaab424cfe60ddb4a7bd4c2a5
qlaurent <quentin-laurent93@hotmail.com>
2019-12-04 16:29:23 +0000

    Removed zsh and other unused parts of the installation script

--- install/create-symlinks.sh
+++ install/create-symlinks.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 #===============================================================================
 #
 #             NOTES: For this to work you must have cloned the github
@@ -20,16 +20,6 @@ sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.bashrc > /dev/null 2>&1
 sudo rm -rf ~/.tmux > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
-# sudo rm -rf ~/.zsh_prompt > /dev/null 2>&1
-sudo rm -rf ~/.zshrc > /dev/null 2>&1
-# sudo rm -rf ~/.gitconfig > /dev/null 2>&1
-# sudo rm -rf ~/.antigen > /dev/null 2>&1
-# sudo rm -rf ~/.antigen.zsh > /dev/null 2>&1
-# sudo rm -rf ~/.psqlrc > /dev/null 2>&1
-# sudo rm -rf ~/.conkyrc > /dev/null 2>&1
-# sudo rm -rf ~/.tigrc > /dev/null 2>&1
-# sudo rm -rf ~/.config > /dev/null 2>&1
-
 #==============
 # Create symlinks in the home folder
 # Allow overriding with files of matching names in the custom-configs dir
@@ -38,40 +28,16 @@ sudo rm -rf ~/.zshrc > /dev/null 2>&1
 ln -sf $dotfiles_dir/vim/vimrc ~/.vimrc
 ln -sf $dotfiles_dir/bash/bashrc ~/.bashrc
 ln -sf $dotfiles_dir/tmux/tmux ~/.tmux
-# ln -sf $dotfiles_dir/zsh/zsh_prompt ~/.zsh_prompt
-ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
-# ln -sf $dotfiles_dir/config ~/.config
-# ln -sf $dotfiles_dir/custom-configs/custom-snips ~/.vim/custom-snips
-
-#if [ -n "$(find $dotfiles_dir/custom-configs -name gitconfig)" ]; then
-#    ln -s $dotfiles_dir/custom-configs/**/gitconfig ~/.gitconfig
-#else
-#    ln -s $dotfiles_dir/gitconfig ~/.gitconfig
-#fi
-
 if [ -n "$(find $dotfiles_dir/custom-configs -name tmux.conf)" ]; then
     ln -s $dotfiles_dir/custom-configs/**/tmux.conf ~/.tmux.conf
 else
     ln -s $dotfiles_dir/linux-tmux/tmux.conf ~/.tmux.conf
 fi
 
-#if [ -n "$(find $dotfiles_dir/custom-configs -name tigrc)" ]; then
-#    ln -s $dotfiles_dir/custom-configs/**/tigrc ~/.tigrc
-#else
-#    ln -s $dotfiles_dir/tigrc ~/.tigrc
-#fi
-
-#if [ -n "$(find $dotfiles_dir/custom-configs -name psqlrc)" ]; then
-#    ln -s $dotfiles_dir/custom-configs/**/psqlrc ~/.psqlrc
-#else
-#    ln -s $dotfiles_dir/psqlrc ~/.psqlrc
-#fi
-
-
 # Install packet management for Vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-sudo apt install build-essential cmake python3-dev
+sudo apt install -y build-essential cmake python3-dev
 
 # YCM compilation
 cd ~/.vim/bundle/YouCompleteMe
@@ -99,14 +65,15 @@ pip install grip
 #fi
 
 #==============
+# NOT USED at the moment
 # Set zsh as the default shell
 #==============
-sudo chsh -s /bin/zsh
+# sudo chsh -s /bin/zsh
 
 #==============
 # Give the user a summary of what has been installed
 #==============
-echo -e "\n====== Summary ======\n"
+# echo -e "\n====== Summary ======\n"
 # cat $log_file
 # echo
 # rm $log_file
